dc4f80ba3ed7c90654a04bce655ae72d7cd1af68
Ulrich Eck <ueck@net-labs.de>
2016-12-13 08:37:09 +0100

    updated to new zmq library naming scheme

--- cmake/FindZMQ.cmake
+++ cmake/FindZMQ.cmake
@@ -1,63 +1,141 @@
-# - Try to find ZMQ
-# Once done this will define
+##=============================================================================
+##
+##  Copyright (c) Kitware, Inc.
+##  All rights reserved.
+##  See LICENSE.txt for details.
+##
+##  This software is distributed WITHOUT ANY WARRANTY; without even
+##  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+##  PURPOSE.  See the above copyright notice for more information.
+##
+##=============================================================================
+# - Try to find ZeroMQ headers and libraries
 #
-#  ZMQ_FOUND - system has ZMQ
-#  ZMQ_INCLUDE_DIRS - the ZMQ include directory
-#  ZMQ_LIBRARIES - Link these to use ZMQ
-#  ZMQ_DEFINITIONS - Compiler switches required for using ZMQ
+# Usage of this module as follows:
 #
-#  Copyright (c) 2011 Lee Hambley <lee.hambley@gmail.com>
+#     find_package(ZeroMQ)
 #
-#  Redistribution and use is allowed according to the terms of the New
-#  BSD license.
-#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# Variables used by this module, they can change the default behaviour and need
+# to be set before calling find_package:
 #
+#  ZeroMQ_ROOT_DIR  Set this variable to the root installation of
+#                            ZeroMQ if the module has problems finding
+#                            the proper installation path.
+#
+# Variables defined by this module:
+#
+#  ZeroMQ_FOUND              System has ZeroMQ libs/headers
+#  ZeroMQ_LIBRARIES          The ZeroMQ libraries
+#  ZeroMQ_INCLUDE_DIR        The location of ZeroMQ headers
+#  ZeroMQ_VERSION            The version of ZeroMQ
 
-if (ZMQ_LIBRARIES AND ZMQ_INCLUDE_DIRS)
-  # in cache already
-  set(ZMQ_FOUND TRUE)
-else (ZMQ_LIBRARIES AND ZMQ_INCLUDE_DIRS)
+set(ZeroMQ_ROOT_DIR ${ZMQ_DIR})
 
-  find_path(ZMQ_INCLUDE_DIR
-    NAMES
-      zmq.h
-    PATHS
-      $ENV{ZMQ_DIR}/include
-      ${ZMQ_DIR}/include
-      /usr/include
-      /usr/local/include
-      /opt/local/include
-      /sw/include
+find_path(ZeroMQ_ROOT_DIR
+  NAMES include/zmq.h
   )
 
-  find_library(ZMQ_LIBRARY
-    NAMES
-      zmq
-    PATHS
-      $ENV{ZMQ_DIR}/lib
-      ${ZMQ_DIR}/lib
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      /sw/lib
-  )
+if(MSVC)
+  #add in all the names it can have on windows
+  if(CMAKE_GENERATOR_TOOLSET MATCHES "v140" OR MSVC14)
+    set(_zmq_TOOLSET "-v140")
+  elseif(CMAKE_GENERATOR_TOOLSET MATCHES "v120" OR MSVC12)
+    set(_zmq_TOOLSET "-v120")
+  elseif(CMAKE_GENERATOR_TOOLSET MATCHES "v110_xp")
+    set(_zmq_TOOLSET "-v110_xp")
+  elseif(CMAKE_GENERATOR_TOOLSET MATCHES "v110" OR MSVC11)
+    set(_zmq_TOOLSET "-v110")
+  elseif(CMAKE_GENERATOR_TOOLSET MATCHES "v100" OR MSVC10)
+    set(_zmq_TOOLSET "-v100")
+  elseif(CMAKE_GENERATOR_TOOLSET MATCHES "v90" OR MSVC90)
+    set(_zmq_TOOLSET "-v90")
+  endif()
 
-  set(ZMQ_INCLUDE_DIRS
-    ${ZMQ_INCLUDE_DIR}
-  )
+  set(_zmq_versions
+     "4_2_0" "4_1_5" "4_1_4" "4_1_3" "4_1_2" "4_1_1" "4_1_0"
+     "4_0_8" "4_0_7" "4_0_6" "4_0_5" "4_0_4" "4_0_3" "4_0_2" "4_0_1" "4_0_0"
+     "3_2_5" "3_2_4" "3_2_3" "3_2_2"  "3_2_1" "3_2_0" "3_1_0")
 
-  if (ZMQ_LIBRARY)
-    set(ZMQ_LIBRARIES
-        ${ZMQ_LIBRARIES}
-        ${ZMQ_LIBRARY}
+  set(_zmq_release_names)
+  set(_zmq_debug_names)
+  foreach( ver ${_zmq_versions})
+    list(APPEND _zmq_release_names "libzmq${_zmq_TOOLSET}-mt-${ver}")
+  endforeach()
+  foreach( ver ${_zmq_versions})
+    list(APPEND _zmq_debug_names "libzmq${_zmq_TOOLSET}-mt-gd-${ver}")
+  endforeach()
+
+  #now try to find the release and debug version
+  find_library(ZeroMQ_LIBRARY_RELEASE
+    NAMES ${_zmq_release_names} zmq libzmq
+    HINTS ${ZeroMQ_ROOT_DIR}/bin
+          ${ZeroMQ_ROOT_DIR}/lib
     )
-  endif (ZMQ_LIBRARY)
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(ZMQ DEFAULT_MSG ZMQ_LIBRARIES ZMQ_INCLUDE_DIRS)
+  find_library(ZeroMQ_LIBRARY_DEBUG
+    NAMES ${_zmq_debug_names} zmq libzmq
+    HINTS ${ZeroMQ_ROOT_DIR}/bin
+          ${ZeroMQ_ROOT_DIR}/lib
+    )
 
-  # show the ZMQ_INCLUDE_DIRS and ZMQ_LIBRARIES variables only in the advanced view
-  mark_as_advanced(ZMQ_INCLUDE_DIRS ZMQ_LIBRARIES)
+  if(ZeroMQ_LIBRARY_RELEASE AND ZeroMQ_LIBRARY_DEBUG)
+    set(ZeroMQ_LIBRARY
+        debug ${ZeroMQ_LIBRARY_DEBUG}
+        optimized ${ZeroMQ_LIBRARY_RELEASE}
+        )
+  elseif(ZeroMQ_LIBRARY_RELEASE)
+    set(ZeroMQ_LIBRARY ${ZeroMQ_LIBRARY_RELEASE})
+  elseif(ZeroMQ_LIBRARY_DEBUG)
+    set(ZeroMQ_LIBRARY ${ZeroMQ_LIBRARY_DEBUG})
+  endif()
 
-endif (ZMQ_LIBRARIES AND ZMQ_INCLUDE_DIRS)
+else()
+  find_library(ZeroMQ_LIBRARY
+    NAMES zmq libzmq
+    HINTS ${ZeroMQ_ROOT_DIR}/lib
+    )
+endif()
 
+find_path(ZeroMQ_INCLUDE_DIR
+  NAMES zmq.h
+  HINTS ${ZeroMQ_ROOT_DIR}/include
+  )
+
+function(extract_version_value value_name file_name value)
+  file(STRINGS ${file_name} val REGEX "${value_name} .")
+  string(FIND ${val} " " last REVERSE)
+  string(SUBSTRING ${val} ${last} -1 val)
+  string(STRIP ${val} val)
+  set(${value} ${val} PARENT_SCOPE)
+endfunction(extract_version_value)
+
+extract_version_value("ZMQ_VERSION_MAJOR" ${ZeroMQ_INCLUDE_DIR}/zmq.h MAJOR)
+extract_version_value("ZMQ_VERSION_MINOR" ${ZeroMQ_INCLUDE_DIR}/zmq.h MINOR)
+extract_version_value("ZMQ_VERSION_PATCH" ${ZeroMQ_INCLUDE_DIR}/zmq.h PATCH)
+
+set(ZeroMQ_VER "${MAJOR}.${MINOR}.${PATCH}")
+
+#We are using the 2.8.10 signature of find_package_handle_standard_args,
+#as that is the version that ParaView 5.1 && VTK 6/7 ship, and inject
+#into the CMake module path. This allows our FindModule to work with
+#projects that include VTK/ParaView before searching for Remus
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  ZeroMQ
+  REQUIRED_VARS ZeroMQ_LIBRARY ZeroMQ_INCLUDE_DIR
+  VERSION_VAR ZeroMQ_VER
+  )
+
+set(ZeroMQ_FOUND ${ZEROMQ_FOUND})
+set(ZeroMQ_INCLUDE_DIRS ${ZeroMQ_INCLUDE_DIR})
+set(ZeroMQ_LIBRARIES ${ZeroMQ_LIBRARY})
+set(ZeroMQ_VERSION ${ZeroMQ_VER})
+
+mark_as_advanced(
+  ZeroMQ_ROOT_DIR
+  ZeroMQ_LIBRARY
+  ZeroMQ_LIBRARY_DEBUG
+  ZeroMQ_LIBRARY_RELEASE
+  ZeroMQ_INCLUDE_DIR
+  ZeroMQ_VERSION
+  )
