1e124e8164f5955271819d7276bfffc80700cbe0
Ulrich Eck <ulrich.eck@tum.de>
2020-03-06 16:28:30 +0100

    try to fix an error that occurs when stopping the dataflow module

--- src/utzmq/ZMQNetwork.cpp
+++ src/utzmq/ZMQNetwork.cpp
@@ -241,10 +241,14 @@ void NetworkModule::stopModule()
 		if (m_ioservice_users.fetch_sub(1, boost::memory_order_release) == 1) {
 			boost::atomic_thread_fence(boost::memory_order_acquire);
 			LOG4CPP_INFO( logger, "Stop IOService" );
-			m_ioserviceKeepAlive->cancel();
-            m_ioservice->stop();
-            m_NetworkThread->join();
-            m_NetworkThread.reset();
+            if (m_ioserviceKeepAlive)
+    			m_ioserviceKeepAlive->cancel();
+            if (m_ioservice)
+                m_ioservice->stop();
+            if (m_NetworkThread ) {
+                m_NetworkThread->join();
+                m_NetworkThread.reset();
+            }
 			m_ioservice.reset();
 		}
 	}
